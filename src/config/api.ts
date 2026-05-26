09c27b1f5fa7d1670b040fd5ba0d9fa553312925
basilio-joseph-lee <manansalajosephlee@gmail.com>
2026-05-26 18:41:35 +0800

    update base URL to production

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 
 // URL ON DEPLOYMENT
-// export const API_BASE_URL = "https://hotel-management-api-x754.onrender.com/api";
+export const API_BASE_URL = "https://hotel-management-api-x754.onrender.com/api";
 
-export const API_BASE_URL = "http://127.0.0.1:8000/api/";
+// export const API_BASE_URL = "http://127.0.0.1:8000/api/";
